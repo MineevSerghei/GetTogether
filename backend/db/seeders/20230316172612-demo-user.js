dfb263ed2943f932c82cfa5eaadc3b307d3d8685
Serghei Mineev <mineevserghei@gmail.com>
2023-03-16 13:48:26 -0400

    Added Users table migration and seeds

---
+++ backend/db/seeders/20230316172612-demo-user.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+
+    options.tableName = 'Users';
+    await queryInterface.bulkInsert(options, [
+      {
+        email: 'demo@email.com',
+        username: 'Demo-user',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'serghei@email.com',
+        username: 'serghei',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'some-other-user@email.com',
+        username: 'some-other-user',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-user', 'serghei', 'some-other-user'] }
+    }, {});
+  }
+};
