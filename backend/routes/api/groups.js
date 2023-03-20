943210aa45b8c39457c6cae28d6b2f1af07244c7
Serghei Mineev <mineevserghei@gmail.com>
2023-03-20 17:24:32 -0400

    Add create group route

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -4,10 +4,36 @@ const { findNumOfMembersAndPreviewImg } = require('../../utils/objects');
 const { Group, GroupImage, Membership, User, Venue } = require('../../db/models');
 const { Op } = require('sequelize');
 
-// const { check } = require('express-validator');
-// const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+const validateGroup = [
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ max: 60, min: 5 })
+        .withMessage('Name must be between 5 and 60 characters'),
+    check('about')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 50 })
+        .withMessage('About must be 50 characters or more'),
+    check('type')
+        .exists({ checkFalsy: true })
+        .isIn(['Online', 'In person'])
+        .withMessage("Type must be 'Online' or 'In person'"),
+    check('private')
+        .exists()
+        .isBoolean()
+        .withMessage('Private must be a boolean'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage('City is required'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('State is required'),
+    handleValidationErrors
+];
 
 // Get all Groups
 router.get('/', async (req, res) => {
@@ -102,7 +128,22 @@ router.get('/:groupId', async (req, res) => {
     return res.json(group);
 });
 
+router.post('/', requireAuth, validateGroup, async (req, res) => {
+    const { name, about, type, private, city, state } = req.body;
 
+    const group = await Group.create({
+        organizerId: req.user.id,
+        name,
+        about,
+        type,
+        private,
+        city,
+        state,
+    });
+
+    res.status(201);
+    return res.json(group);
+});
 
 
 
